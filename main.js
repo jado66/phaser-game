974c591159147d8f126236ca6d933a938171149e
Jadon Erwin <Jadon.Erwin@us.kbr.com>
2024-06-19 22:38:05 -0400

    Added circle with keyboard controls

--- main.js
+++ main.js
@@ -11,15 +11,47 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+let player;
+let cursors;
+
 function preload() {
     // Load assets here
 }
 
 function create() {
     // Initialize your game scene here
-    this.add.text(100, 100, 'Hello, Phaser!', { fill: '#0f0' });
+    // Create a circle in the middle of the screen
+    player = this.add.circle(400, 300, 25, 0xffffff);
+
+    // Enable cursor keys for movement
+    cursors = this.input.keyboard.createCursorKeys();
+
+    // Enable WASD keys for movement
+    this.wasdKeys = this.input.keyboard.addKeys({
+        up: Phaser.Input.Keyboard.KeyCodes.W,
+        down: Phaser.Input.Keyboard.KeyCodes.S,
+        left: Phaser.Input.Keyboard.KeyCodes.A,
+        right: Phaser.Input.Keyboard.KeyCodes.D
+    });
 }
 
 function update() {
-    // Game loop logic here
+    let speed = 200;
+    let moveX = 0;
+    let moveY = 0;
+
+    if (cursors.left.isDown || this.wasdKeys.left.isDown) {
+        moveX = -speed;
+    } else if (cursors.right.isDown || this.wasdKeys.right.isDown) {
+        moveX = speed;
+    }
+
+    if (cursors.up.isDown || this.wasdKeys.up.isDown) {
+        moveY = -speed;
+    } else if (cursors.down.isDown || this.wasdKeys.down.isDown) {
+        moveY = speed;
+    }
+
+    player.x += moveX * (this.game.loop.delta / 1000);
+    player.y += moveY * (this.game.loop.delta / 1000);
 }

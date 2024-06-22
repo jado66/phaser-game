c238a7a62cfdc8d550610caea58d8f964a3e88cf
Jadon Erwin <jadon.erwin@sinch.com>
2024-06-21 20:10:17 -0600

    relative links for phaser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Phaser from "phaser";
+import Phaser from "../node_modules/phaser";
 import Game from './scenes/Game.js';
 
 export const VW = 1400 //Viewport width
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,4 +1,4 @@
-import Phaser from "phaser";
+import Phaser from "../node_modules/phaser";
 import { VW, VH } from "..";
 import { addCenterLines } from "../debug/addCenterLines";
 

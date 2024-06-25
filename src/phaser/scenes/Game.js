4232e96d5a40b04dc634f2469edbd134ef698a6b
Tanner McNatt <tannermcmanner@gmail.com>
2024-06-24 19:23:33 -0600

    added sprite

--- src/phaser/scenes/Game.js
+++ src/phaser/scenes/Game.js
@@ -8,13 +8,13 @@ import { addPlayerCollisionBounds } from "../debug/addPlayerCollisionBounds";
 import { Berry } from "../game-components/dropped-items/Berry";
 import { Monster } from "../engine/Monster";
 
-let cursors
-export let player
+let cursors;
+export let player;
 
 export default class Game extends Phaser.Scene {
     constructor() {
         super("game");
-        this.debug = globalDebug
+        this.debug = globalDebug;
     }
     init() {
     }
@@ -23,41 +23,36 @@ export default class Game extends Phaser.Scene {
         this.load.image('tiles', '../assets/dungeon_tiles16.png');
         this.load.image('berryTexture','../assets/berryTexture.png')
         this.load.image('monsterTexture','../assets/monster.png')
+        this.load.image('spriteTextureForward','../assets/sprite-forward.png')
         this.load.tilemapTiledJSON('map', '../assets/map2.json');
 
         this.gameUI = new GameUI(this);
         this.gameUI.preload();
-
     }
     create() {
 
-        const map = this.make.tilemap({ key: 'map' })
-        const tileset = map.addTilesetImage('dungeon', 'tiles')
-        const worldLayer = map.createLayer('world', tileset)
+        const map = this.make.tilemap({ key: 'map' });
+        const tileset = map.addTilesetImage('dungeon', 'tiles');
+        const worldLayer = map.createLayer('world', tileset);
 
-        this.worldLayer = worldLayer
+        this.worldLayer = worldLayer;
         this.worldLayer.setScale(4);
 
         this.worldLayer.setCollisionByProperty({ collides: true });
 
-        const playerSize = 50
-        const halfPlayerSize = playerSize/2
+        const playerSize = 50;
+        const halfPlayerSize = playerSize / 2;
 
-        player = this.add.polygon(VW/2, VH/2, [0, -halfPlayerSize, halfPlayerSize, halfPlayerSize, -halfPlayerSize, halfPlayerSize], 0xffffff);
-
-        player.setOrigin(0, 3/halfPlayerSize); // move the player up just a tidge
+        player = this.add.sprite(VW / 2, VH / 2, 'spriteTextureForward');
 
         this.physics.add.existing(player);
-        player.body.setSize(50, 50); // Set the size of the physics body
-        player.body.setOffset(-25, -25); // Offset to center the collider
-
-        // player.body.setCollideWorldBounds(true);
+        player.body.setSize(50, 50);
 
         this.physics.add.collider(player, this.worldLayer);
 
         const inventory = player.inventory = new Inventory();
-        player.health = 100
-        player.stamina = 100
+        player.health = 100;
+        player.stamina = 100;
 
         player.takeDamage = (amount) => {
             player.health -= amount;
@@ -69,20 +64,17 @@ export default class Game extends Phaser.Scene {
         };
 
         this.restart = () => {
-            // Logic to reset the game state and restart the scene
-            this.scene.restart(); // This will restart the scene
+            this.scene.restart();
         };
 
         this.gameUI.create(player);
 
-        this.input.on('pointermove', function (pointer) {
-            //Make sure we aren't strafing            
-            // Calculate the angle between the player and the pointer
-            const angle = Phaser.Math.Angle.Between(player.x, player.y, pointer.worldX, pointer.worldY);
-            
-          
-            // Set the player's rotation to this angle
-            player.rotation = angle + Math.PI / 2;
+        // To store the last known position of the cursor
+        this.pointerPosition = { x: player.x, y: player.y };
+
+        this.input.on('pointermove', pointer => {
+            this.pointerPosition.x = pointer.worldX;
+            this.pointerPosition.y = pointer.worldY;
         });
 
         // Enable cursor keys for movement
@@ -96,48 +88,49 @@ export default class Game extends Phaser.Scene {
             right: Phaser.Input.Keyboard.KeyCodes.D
         });
 
-        this.shiftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT)
+        this.shiftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
 
-        if (this.debug){
-            addCenterLines(this)
-            addPlayerCollisionBounds(this)
-            enableCameraZoom(this)
-            addDebugTileCollisionColors(this, this.worldLayer)
+        if (this.debug) {
+            addCenterLines(this);
+            addPlayerCollisionBounds(this);
+            enableCameraZoom(this);
+            addDebugTileCollisionColors(this, this.worldLayer);
         }
-        
+
         // Set camera to follow the player
         this.cameras.main.startFollow(player);
 
         // Define deadzone for the camera to only move when player reaches edges
         this.cameras.main.setDeadzone(VW / 2, VH / 2);
 
+        const berries = createRandomBerries(this, this.worldLayer, 10);
 
-        const berries = createRandomBerries(this, this.worldLayer, 10)
-
-        const monster1 = new Monster(this, 200, 200, 'monsterTexture', 'Monster1')
-        const monster2 = new Monster(this, 202, 300, 'monsterTexture', 'Monster2')
-        this.monsters = [monster1, monster2]
+        const monster1 = new Monster(this, 200, 200, 'monsterTexture', 'Monster1');
+        const monster2 = new Monster(this, 202, 300, 'monsterTexture', 'Monster2');
+        this.monsters = [monster1, monster2];
 
     }
     update(time, delta) {
 
-        this.gameUI.update()
+        this.gameUI.update();
 
         this.monsters.forEach(character => character.update(time, delta));
 
         let speed = 200;
 
-        
-
         let moveX = 0;
         let moveY = 0;
-    
+
+        // Update the player's rotation based on the stored cursor position
+        const pointerPosition = this.pointerPosition;
+        const angle = Phaser.Math.Angle.Between(player.x, player.y, pointerPosition.x, pointerPosition.y);
+        player.rotation = angle + Math.PI / 2;
 
         const playerAngle = player.rotation - Math.PI / 2; // Adjust rotation since we added PI/2 during the update
 
         if (this.shiftKey.isDown && player.stamina >= 0 && (cursors.up.isDown || this.wasdKeys.up.isDown)){
-            player.stamina -= 1 * this.game.loop.delta / 1000 * 50
-            speed = speed * 2
+            player.stamina -= 1 * this.game.loop.delta / 1000 * 50;
+            speed = speed * 2;
         }
 
         // Movement towards or away from the cursor
@@ -157,19 +150,18 @@ export default class Game extends Phaser.Scene {
             moveX -= Math.cos(playerAngle + Math.PI / 2) * speed;
             moveY -= Math.sin(playerAngle + Math.PI / 2) * speed;
         }
-    
-        if (player.stamina < 100){
 
+        if (player.stamina < 100) {
             if (!moveX && !moveY){
-                player.stamina += 1 * this.game.loop.delta / 1000 * 5
-            }
-            else if (player.stamina > 10){
-                player.stamina += 1 * this.game.loop.delta / 1000 * 2.5
+                player.stamina += 1 * this.game.loop.delta / 1000 * 5;
+            } else if (player.stamina > 10){
+                player.stamina += 1 * this.game.loop.delta / 1000 * 2.5;
             }
         }
-       
 
-    // Apply the calculated movements to the player's position.
+        player.setAngle(0);
+
+        // Apply the calculated movements to the player's position.
         player.body.setVelocityX(moveX * (this.game.loop.delta / 20));
         player.body.setVelocityY(moveY * (this.game.loop.delta / 20));
     }
@@ -184,9 +176,9 @@ function getRandomPosition(scene, worldLayer) {
       tile = worldLayer.getTileAtWorldXY(x, y);
     } while (tile && tile.properties.collides);
     return { x, y };
-  }
+}
   
-  function createRandomBerries(scene, worldLayer, count) {
+function createRandomBerries(scene, worldLayer, count) {
     const berries = [];
     for (let i = 0; i < count; i++) {
       const { x, y } = getRandomPosition(scene, worldLayer);
@@ -194,7 +186,6 @@ function getRandomPosition(scene, worldLayer) {
       berry.setScale(4); // Scale the berry by a factor of 4
 
       berries.push(berry);
-
     }
     return berries;
-  }
+}

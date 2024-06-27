402101ae5fd0765918671c515e130e17a01da61c
Jadon Erwin <jadon.erwin@sinch.com>
2024-06-27 12:19:47 -0600

    NavMesh was totally screwing it up

--- src/phaser/PhaserGame.jsx
+++ src/phaser/PhaserGame.jsx
@@ -1,48 +1,52 @@
 'use client';
 
-import { PhaserNavMeshPlugin } from "phaser-navmesh";
 import React, { useEffect, useState } from 'react';
-import Game from './scenes/Game';
 
-export const VW = 1400; // Viewport width
-export const VH = 600;  // Viewport height
+export const VW = 1400;
+export const VH = 600;
 
 const PhaserGame = () => {
   const [gameInstance, setGameInstance] = useState(null);
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      const config = {
-        type: Phaser.AUTO,
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#AFE1AF',
-        scene: [Game],
-        parent: 'phaser-game',
-        physics: {
-          default: 'arcade',
-        },
-        plugins: {
-          scene: [
-            {
-              key: "NavMeshPlugin",
-              plugin: PhaserNavMeshPlugin,
-              mapping: "navMeshPlugin",
-              start: true,
-            },
-          ],
-        },
-        callbacks: {
-          postBoot: function (game) {
-            game.canvas.style.width = '100%';
-            game.canvas.style.height = '100%';
-          }
-        }
-      };
-
       import('phaser').then((Phaser) => {
-        const game = new Phaser.Game(config);
-        setGameInstance(game);
+        import('phaser-navmesh').then(({ PhaserNavMeshPlugin }) => {
+          import('./scenes/Game').then((GameModule) => {
+            const Game = GameModule.default;
+
+            const config = {
+              type: Phaser.AUTO,
+              width: '100%',
+              height: '100%',
+              backgroundColor: '#AFE1AF',
+              scene: [Game],
+              parent: 'phaser-game',
+              physics: {
+                default: 'arcade',
+              },
+              plugins: {
+                scene: [
+                  {
+                    key: "NavMeshPlugin",
+                    plugin: PhaserNavMeshPlugin,
+                    mapping: "navMeshPlugin",
+                    start: true,
+                  },
+                ],
+              },
+              callbacks: {
+                postBoot: function (game) {
+                  game.canvas.style.width = '100%';
+                  game.canvas.style.height = '100%';
+                }
+              }
+            };
+
+            const game = new Phaser.Game(config);
+            setGameInstance(game);
+          });
+        });
       });
     }
 
@@ -55,15 +59,15 @@ const PhaserGame = () => {
 
   return (
     <div 
-        id="phaser-game" 
-        style={{
-            borderRadius:'2.5em', 
-            overflow:'hidden',
-            boxShadow: 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px',
-            border:'1px solid black',
-            width:'100%',
-            height:'100%'
-        }}
+      id="phaser-game" 
+      style={{
+        borderRadius:'2.5em', 
+        overflow:'hidden',
+        boxShadow: 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px',
+        border:'1px solid black',
+        width:'100%',
+        height:'100%'
+      }}
     />
   );
 };
